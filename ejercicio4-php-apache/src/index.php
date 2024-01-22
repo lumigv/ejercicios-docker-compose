107f6af7628cf43369d95fcd471732983464b0d9
lumigv <lumigv@gmail.com>
2024-01-22 14:46:25 +0100

    Inicio repositorio

---
+++ ejercicio4-php-apache/src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo docker compose</title>
+</head>
+<body>
+    <?php echo "<h1>Ejemplo docker compose con PHP</h1>"; ?>   
+</body>
+</html>
+
+
